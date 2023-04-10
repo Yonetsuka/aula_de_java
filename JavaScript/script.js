214ee29ee60c5ac4788bf31d9ba9ac7542b750ca
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2023-04-10 11:49:18 -0300

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,30 @@
+let novo = document.getElementById('idNome')
+
+function mudar()
+{
+    document.getElementById('titulo').innerHTML = novo.value
+}
+
+document.write('<br>')
+
+/*while
+
+let cont = 0
+while(cont < 10)
+{
+    document.write('O valor do while é: '+ cont)
+    cont++
+    document.write('<br>')
+}
+
+//do while
+
+let cont1 = 0
+
+do
+{
+    document.write('O valor é: ' + cont1)
+    cont1++
+    document.write('<br>')
+}while(cont1 ==0)
+*/
